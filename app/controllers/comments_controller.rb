e20114765b5b6bfc98c83b3dae6e9b9efea0803a
loi.super <loi.ng@qq.com>
2021-04-30 23:22:02 +0800

    Deleting Comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,14 +5,15 @@ class CommentsController < ApplicationController
       redirect_to article_path(@article)
     end
   
-    private
-      def article_params
-        params.require(:article).permit(:title, :body, :status)
-      end
-
+    def destroy
+      @article = Article.find(params[:article_id])
+      @comment = @article.comments.find(params[:id])
+      @comment.destroy
+      redirect_to article_path(@article)
+    end
+  
     private
       def comment_params
-        params.require(:comment).permit(:commenter, :body, :status)
+        params.require(:comment).permit(:commenter, :body)
       end
-end
-  
+end  
